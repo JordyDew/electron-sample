40db6db9ae001df2c8e1e671fd525346c5231424
Jordy Deweer <jordydeweer@hotmail.com>
2018-05-19 20:38:26 +0200

    Add source code of the c# executable

---
+++ bin/src/cs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || args[0].Equals(""))
+                Console.WriteLine("Yo whazzup?!  C# is awesome!");
+            else
+                Console.WriteLine("Hey " + args[0] + "!  This program was written in C#.  Definitely better than WinForms IMO. *smile*");
+        }
+    }
+}
